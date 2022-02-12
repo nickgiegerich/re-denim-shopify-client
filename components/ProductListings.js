da62010199b995e14a7d4634eb98aa9308eea062
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2022-02-12 10:47:51 -0700

    add hero, add about us

--- components/ProductListings.js
+++ components/ProductListings.js
@@ -1,16 +1,18 @@
-import ProductCard from '@/components/ProductCard'
+import ProductCard from "@/components/ProductCard";
 
 function ProductListings({ products }) {
- 
   return (
-    <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
-      {
-        products.map((product, index) => (
+    <div className="mt-20 md:mt-10">
+      <div className="text-center text-4xl font-primary font-semibold">
+        Our Current Collection
+      </div>
+      <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
+        {products.map((product, index) => (
           <ProductCard key={index} product={product} />
-        ))
-      }
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProductListings
+export default ProductListings;
--- components/StoreHeading.js
+++ components/StoreHeading.js
@@ -1,16 +1,36 @@
-import PageTitle from '@/components/PageTitle'
+import PageTitle from "@/components/PageTitle";
 
 function StoreHeading() {
   return (
-    <div className="">
-      <PageTitle text="Tailoring - Alterations - Embroidery" />
-      <p className="max-w-xl text-center px-2 mx-auto text-lg font-light text-gray-600">
-        Denim Re-purposed into Fashionable Accessories <br/>
-        Custom Embroidery in <span className='inline-block text-palette-primary font-bold text-xl leading-3 pb-2'>Vivid Colors</span> <br/>
-        Totes, Satchels, Carry-ons, Purses, Packs and more!
-      </p>
+    <div className="h-screen">
+      <div className="bg-hero rounded-lg h-1/3 md:h-1/2">
+        <PageTitle text="more than just bags." />
+        {/* <p className="max-w-xl text-center px-2 mx-auto text-lg font-normal text-gray-100">
+          Denim Re-purposed into Fashionable Accessories <br />
+          Custom Embroidery in{" "}
+          <span className="inline-block text-palette-primary font-bold text-xl leading-3 pb-2">
+            Vivid Colors
+          </span>{" "}
+          <br />
+          Totes, Satchels, Carry-ons, Purses, Packs and more!
+        </p> */}
+      </div>
+      <div className="h-2/3 md:h-1/2 flex flex-col md:flex-row justify-evenly">
+        <div className="w-full text-center mt-20">
+          <span className="text-6xl font-primary font-semibold leading-relaxed">Why Us?</span>
+        </div>
+        <div className="w-full text-righ md:mt-20 px-10">
+          <span className="text-2xl leading-relaxed">
+            At ReDenim each creation is unique, no two creations are identical.
+            This uniqueness is driven by the fact that no two pairs of jeans are
+            the same size, shape, or patena. Truly one of a kind creations that
+            are durable, look great and contribute to improving the world around
+            us, one product at a time.
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default StoreHeading
+export default StoreHeading;
